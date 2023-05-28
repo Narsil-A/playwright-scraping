e970ca7532d7b0ec6f92f466606b2dccec1261ed
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-05-27 20:55:02 -0400

    testing binance news, ncbi database protein

--- sephora/sephora/spiders/sephora_skin_care.py
+++ sephora/sephora/spiders/sephora_skin_care.py
@@ -6,25 +6,27 @@ class SephoraSkinCareSpider(scrapy.Spider):
     
     custom_settings = {
         'FEEDS':{
-            'sephoradata.json':{'format':'json', 'overwrite':True}
+            'pdbdata.json':{'format':'json', 'overwrite':True}
         }
     }
 
     def start_requests(self):
-        yield scrapy.Request('https://www.sephora.com/shop/skincare', 
+        yield scrapy.Request('https://www.rcsb.org/search?request=%7B%22query%22%3A%7B%22type%22%3A%22group%22%2C%22nodes%22%3A%5B%7B%22type%22%3A%22group%22%2C%22nodes%22%3A%5B%7B%22type%22%3A%22group%22%2C%22nodes%22%3A%5B%7B%22type%22%3A%22terminal%22%2C%22service%22%3A%22text%22%2C%22parameters%22%3A%7B%22attribute%22%3A%22rcsb_entry_info.structure_determination_methodology%22%2C%22operator%22%3A%22exact_match%22%2C%22value%22%3A%22experimental%22%7D%7D%5D%2C%22logical_operator%22%3A%22and%22%7D%5D%2C%22logical_operator%22%3A%22and%22%2C%22label%22%3A%22text%22%7D%5D%2C%22logical_operator%22%3A%22and%22%7D%2C%22return_type%22%3A%22entry%22%2C%22request_options%22%3A%7B%22scoring_strategy%22%3A%22combined%22%2C%22results_content_type%22%3A%5B%22experimental%22%5D%2C%22paginate%22%3A%7B%22start%22%3A0%2C%22rows%22%3A25%7D%2C%22sort%22%3A%5B%7B%22sort_by%22%3A%22score%22%2C%22direction%22%3A%22desc%22%7D%5D%7D%2C%22request_info%22%3A%7B%22query_id%22%3A%22e0fff76e6009d1aefc3970505b66f430%22%7D%7D', 
         meta= dict(
             playwright = True,
             playwright_include_page = True,
             playwright_page_methods = [
-                PageMethod('wait_for_selector', 'div#css-1322gsb')
+                PageMethod('wait_for_selector', 'div.results-item')
             ]
         ))
 
 
     async def parse(self, response):
 
-        for product in response.css('a.css-klx76'): # this selector is the problem
+        for product in response.css('div.results-item'): # this selector is the problem
             
             yield {
-            'title':product.css('span.css-12z2u5.eanm77i0::text').get()
+            'title':product.css('h4 a::::text').get(),
+            'link':product.css('a::attr(href)::text').get(),
+            'authors':product.css('p a span::text').get()
         }
